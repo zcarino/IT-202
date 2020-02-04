9bdd580814f1580f533c0fcd73f64df00ad05133
zcarino <zxpro77@gmail.com>
2020-02-03 23:48:43 -0500

    update

---
+++ RegistrationPractice/config.php
@@ -0,0 +1,7 @@
+<?php
+//please don't copy my ucid
+$dbuser = "zjc6";//your ucid
+$dbpass = "Zcarzxpro779!";//your phpMyAdmin password
+$dbhost = "sql1.njit.edu";//whichever server you login to from web.njit.edu/mysql/phpMyAdmin
+$dbdatabase = "zjc6";//your ucid
+?>
