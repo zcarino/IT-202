7919f6daf6011e858072a94bbd9f303b023740e4
zcarino <zxpro77@gmail.com>
2020-03-05 23:18:58 -0500

    added order.sql

---
+++ project/Address.sql
@@ -0,0 +1,9 @@
+CREATE TABLE IF NOT EXISTS `UserAddresses` (
+                            `User_ID` int auto_increment not null,
+                            `Order_Number` int auto_increment not null,
+                            `City` varchar(60) not null,
+                            `Address` varchar(60) not null,
+                            `PostalCode` varchar(5) not null,
+                            `date_created` timestamp not null default current_timestamp,
+                            PRIMARY KEY (`User_id`)
+                            ) CHARACTER SET utf8 COLLATE utf8_general_ci
