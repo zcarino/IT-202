0a52066e964cb8f3e4a839d13ca0d836552a3c64
zcarino <zxpro77@gmail.com>
2020-05-15 00:56:18 -0400

    Fixed Delete and cart now shows item details

--- project/delete.php
+++ project/delete.php
@@ -1,15 +1,18 @@
 <?php
 error_reporting( E_ALL );
 ini_set( 'display_errors', 1 );  
-if(isset($_POST["product_id"]) && !empty($_POST["product_id"])){
+if(isset($_POST["id"]) && !empty($_POST["id"])){
     require("config.php");
     $connection_string = "mysql:host=$dbhost;dbname=$dbdatabase;charset=utf8mb4";
     $db = new PDO($connection_string, $dbuser, $dbpass);
-    $query = "DELETE FROM products WHERE product_id = :product_id";
-
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $param_id = trim($_POST["id"]);
+    $query = ("DELETE FROM products WHERE product_id = (".$param_id.")");
     if($stmt = $db->prepare($query)){
+        
         $stmt->bindParam(":product_id", $param_id, PDO::PARAM_INT);
-        $param_id = trim($_POST["product_id"]);
+        
+
         if($stmt->execute()){
             header("location: inventory.php");
             exit();
@@ -40,7 +43,7 @@ if(isset($_POST["product_id"]) && !empty($_POST["product_id"])){
                     </div>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="alert alert-danger fade in">
-                            <input type="hidden" name="product_id" value="<?php echo trim($_GET["product_id"]); ?>"/>
+                            <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
                             <p>Are you sure you want to delete this record?</p><br>
                             <p>
                                 <input type="submit" value="Yes" class="btn btn-danger">
